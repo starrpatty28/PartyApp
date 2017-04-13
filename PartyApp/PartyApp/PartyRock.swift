538ee80372296b6576b9dab01388348eeb2b1ae7
starrpatty28 <starrpatty28@gmail.com>
2017-04-13 15:31:36 -0700

    set an array of stings in PartyRock.swift file

---
+++ PartyApp/PartyApp/PartyRock.swift
@@ -0,0 +1,26 @@
+//
+//  PartyRock.swift
+//  PartyApp
+//
+//  Created by Noi-Ariella Baht Israel on 4/13/17.
+//  Copyright © 2017 Plant A Seed of Code. All rights reserved.
+//
+
+import Foundation
+
+
+class PartyRock {
+    private var _imageURL: String!
+    private var _videoURL: String!
+    private var _videoTitle: String!
+    
+    
+    
+    init(imageURL: String, videoURL: String, videoTitle: String) {
+        
+        _imageURL = imageURL
+        _videoURL = videoURL
+        _videoTitle = videoTitle
+    }
+}
+
