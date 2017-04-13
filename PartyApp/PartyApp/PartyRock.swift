2540ebf36c0642430b4034a605068d2e83a09abf
starrpatty28 <starrpatty28@gmail.com>
2017-04-13 15:33:13 -0700

    set getters for URL

--- PartyApp/PartyApp/PartyRock.swift
+++ PartyApp/PartyApp/PartyRock.swift
@@ -14,6 +14,17 @@ class PartyRock {
     private var _videoURL: String!
     private var _videoTitle: String!
     
+    var imageURL: String {
+        return _imageURL
+    }
+    
+    var videoURL: String {
+        return _videoURL
+    }
+    
+    var videoTitle: String {
+        return _videoTitle
+    }
     
     
     init(imageURL: String, videoURL: String, videoTitle: String) {
