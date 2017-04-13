8c2d51ccd10fd8bcee270bae542f0299d05ac2f2
starrpatty28 <starrpatty28@gmail.com>
2017-04-12 22:52:27 -0700

    add new UITableView Cell

---
+++ PartyApp/PartyApp/PartyCell.swift
@@ -0,0 +1,19 @@
+//
+//  PartyCell.swift
+//  PartyApp
+//
+//  Created by Noi-Ariella Baht Israel on 4/12/17.
+//  Copyright © 2017 Plant A Seed of Code. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+
+}
